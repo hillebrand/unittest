51584e32a82f36dc4dc317bed6ef623c7f557c97
hgelderb <hillebrandgelderblom@eleven.nl>
2018-12-12 11:02:58 +0100

    Third test fix

--- src/main/java/Adder.java
+++ src/main/java/Adder.java
@@ -1,6 +1,9 @@
 public class Adder {
 
-	public int add(int a, int b) {
-		return a + b;
+	public int add(int... args) {
+		int a = args[0];
+		int b = args[1];
+		int c = args.length == 3 ? args[2] : 0;
+		return a + b + c;
 	}
 }
